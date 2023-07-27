a0c996dde617fd4bf5a47286a6ae7f58ff0e1c95
Valeriya Zhuravleva <btears777@gmail.com>
2023-07-27 10:40:14 +0300

    fix sizes

--- app/src/main/java/com/example/weatherapp/presentation/components/WeekCards.kt
+++ app/src/main/java/com/example/weatherapp/presentation/components/WeekCards.kt
@@ -1,8 +1,7 @@
 package com.example.weatherapp.presentation.components
 
 import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -12,12 +11,15 @@ import com.example.weatherapp.presentation.WeatherState
 fun WeekCards(state: WeatherState){
     val scrollState = rememberScrollState()
     Row(modifier = Modifier
-        .fillMaxSize()
+        .fillMaxWidth()
         .horizontalScroll(state = scrollState)){
         state.weather.weekWeather.forEach{
-                day -> WeekDayCard(weekDay = day.dayOfWeek, date = day.date,
-            id = day.icon, weather = day.condition,
-            minMaxTemp = "${day.maxTemperature.toInt()}/${day.minTemperature.toInt()}°C")
+                day -> WeekDayCard(
+                    weekDay = day.dayOfWeek, date = day.date,
+                    id = day.icon, weather = day.condition,
+                    minMaxTemp = "${day.maxTemperature.toInt()}/${day.minTemperature.toInt()}°C"
+                )
+
         }
     }
 }
